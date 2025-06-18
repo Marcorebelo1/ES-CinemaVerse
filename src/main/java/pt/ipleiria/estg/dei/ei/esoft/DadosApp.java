2aa9379e98aa818dd9fe342cc6e36b60b3b427e6
Marcorebelo1 <marcorebelo2062005@gmail.com>
2025-06-18 20:00:30 +0100

    Classes Iniciais

---
+++ src/main/java/pt/ipleiria/estg/dei/ei/esoft/DadosApp.java
@@ -0,0 +1,39 @@
+package pt.ipleiria.estg.dei.ei.esoft;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DadosApp {
+    private static DadosApp instance = null;
+
+//    private List<Livro> livros = new ArrayList<>();
+
+    private DadosApp() {
+//        livros.add(new Livro("Ensaio Sobre a Cegueira", "José Saramago", "9789722113201"));
+//        livros.add(new Livro("Os Maias", "Eça de Queirós", "9789723713660"));
+//        livros.add(new Livro("Memorial do Convento", "José Saramago", "9789722113256"));
+//        livros.add(new Livro("A Cidade e as Serras", "Eça de Queirós", "9789723715954"));
+//        livros.add(new Livro("Livro do Desassossego", "Fernando Pessoa", "9789727086494"));
+//        livros.add(new Livro("Amor de Perdição", "Camilo Castelo Branco", "9789897023476"));
+//        livros.add(new Livro("A Costa dos Murmúrios", "Lídia Jorge", "9789722117001"));
+//        livros.add(new Livro("A Sibila", "Agustina Bessa-Luís", "9789722115632"));
+//        livros.add(new Livro("Jerusalém", "Gonçalo M. Tavares", "9789726081940"));
+//        livros.add(new Livro("Nenhum Olhar", "José Luís Peixoto", "9789720048505"));
+    }
+
+    public static DadosApp getInstance() {
+        if (instance == null) {
+            instance = new DadosApp();
+            carregarDados();
+        }
+        return instance;
+    }
+
+    private static void carregarDados() {}
+
+    private static void gravarDados() {}
+
+//    public List<Livro> getLivros() {
+//        return livros;
+//    }
+}
---
+++ src/main/java/pt/ipleiria/estg/dei/ei/esoft/classes/Sala.java
@@ -0,0 +1,4 @@
+package pt.ipleiria.estg.dei.ei.esoft.classes;
+
+public class Sala {
+}
